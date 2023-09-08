7b40529906406d40b4e30c7d1d27181e63bf2ab8
Andrey Kozel <andrey.kozel@spanning.com>
2023-09-08 16:06:17 +0300

    Added initialization of the queues

--- worker-cli/src/service/QueueService.ts
+++ worker-cli/src/service/QueueService.ts
@@ -2,6 +2,7 @@
  * Copyright (C) 2023 Spanning Cloud Apps.  All rights reserved.
  */
 import config from 'config';
+import { SQSClient, CreateQueueCommand } from '@aws-sdk/client-sqs';
 
 export default class QueueService {
 
@@ -9,17 +10,52 @@ export default class QueueService {
   private readonly workflowResultQueue: string;
   private readonly workflowStepInteractionRequestQueue: string;
   private readonly workflowStepInteractionResultQueue: string;
+  private readonly sqsClient: SQSClient;
 
   constructor() {
-    const prefix = config.get('sqs.prefix');
-    this.workflowRequestQueue = `${prefix}_${config.get('sqs.queues.workflowRequestQueue')}`;
-    this.workflowResultQueue = `${prefix}_${config.get('sqs.queues.workflowResultQueue')}`;
-    this.workflowStepInteractionRequestQueue = `${prefix}_${config.get('sqs.queues.workflowStepInteractionRequestQueue')}`;
-    this.workflowStepInteractionResultQueue = `${prefix}_${config.get('sqs.queues.workflowStepInteractionResultQueue')}`;
+    const prefix = config.get('aws.sqs.prefix');
+    this.workflowRequestQueue = `${prefix}_${config.get('aws.sqs.queues.workflowRequestQueue')}`;
+    this.workflowResultQueue = `${prefix}_${config.get('aws.sqs.queues.workflowResultQueue')}`;
+    this.workflowStepInteractionRequestQueue = `${prefix}_${config.get('aws.sqs.queues.workflowStepInteractionRequestQueue')}`;
+    this.workflowStepInteractionResultQueue = `${prefix}_${config.get('aws.sqs.queues.workflowStepInteractionResultQueue')}`;
+
+    const region: string = config.get('aws.region');
+    const endpoint: string = config.get('aws.localstack.endpoint')
+    this.sqsClient = new SQSClient({ region, endpoint });
   }
 
   public async initialize_queues() {
+    try {
+      console.log(`Creating queue ${this.workflowRequestQueue}`);
+      const command = new CreateQueueCommand({ QueueName: this.workflowRequestQueue });
+      await this.sqsClient.send(command);
+    } catch(e) {
+      console.error(`Failed ot create queue`, e);
+    }
 
+    try {
+      console.log(`Creating queue ${this.workflowResultQueue}`);
+      const command = new CreateQueueCommand({ QueueName: this.workflowResultQueue });
+      await this.sqsClient.send(command);
+    } catch(e) {
+      console.error(`Failed ot create queue`, e);
+    }
+
+    try {
+      console.log(`Creating queue ${this.workflowStepInteractionRequestQueue}`);
+      const command = new CreateQueueCommand({ QueueName: this.workflowStepInteractionRequestQueue });
+      await this.sqsClient.send(command);
+    } catch(e) {
+      console.error(`Failed ot create queue`, e);
+    }
+
+    try {
+      console.log(`Creating queue ${this.workflowStepInteractionResultQueue}`);
+      const command = new CreateQueueCommand({ QueueName: this.workflowStepInteractionResultQueue });
+      await this.sqsClient.send(command);
+    } catch(e) {
+      console.error(`Failed ot create queue`, e);
+    }
   }
 
   public async subscribeToWorkflows() {
